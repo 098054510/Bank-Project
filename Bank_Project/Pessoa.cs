ad3b5d875dc6210322d77c182fdba7bf52bac50c
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:52:29 -0300

    Bank Project made in C#

---
+++ Bank_Project/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Bank_Project
+{
+    class Pessoa
+    {
+        public string nome { get; set; }
+        public int NumeroDeConta { get; private set; }
+        public double Saldo { get; set; }
+        public double Saque { get; private set; }
+        public double Deposito { get; set; }
+
+        public Pessoa(string nome, double Saldo)
+        {
+            this.nome = nome;
+            Saldo = Saldo;
+        }
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+            Saldo = 0.0;
+        }
+        public void Sacar(double quantia)
+        {
+            Saldo = Saldo - quantia - 5.00;
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo = Saldo + quantia;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " +
+                NumeroDeConta +
+                "Associado: " +
+                nome +
+                "Saldo: " +
+                Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Bank_Project/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa P;
+            string resp, resp3, resp4;
+
+            Console.WriteLine("Digite o nome do associado.");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe o número da conta.");
+            Console.Write("Conta :");
+            int NumeroDeConta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Fazer Deposito inicial? [S/N]");
+            resp = Console.ReadLine();
+
+            P = new Pessoa(nome);
+            if (resp == "s")
+            {
+                Console.WriteLine("insira o valor a ser depositado.");
+                Console.Write("Deposito: R$");
+                double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                P.Depositar(quantia);
+                Console.WriteLine("Conta Criada! Associado: " + P.nome);
+                Console.WriteLine("Conta: " + NumeroDeConta);
+                Console.WriteLine("Total em Conta: R$" + P.Saldo);
+            }
+            else if (resp == "n")
+            {
+                Console.WriteLine("Informações de Conta: " + P.nome + ", Conta N° " + NumeroDeConta);
+                Console.WriteLine("Saldo Atual: R$" + P.Saldo);
+            }
+
+            Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+            resp3 = Console.ReadLine();
+            if (resp3 == "s")
+            {
+                Console.WriteLine("Qual operação deseja fazer? ");
+                Console.WriteLine("Deposito [1]");
+                Console.WriteLine("Saque [2]");
+                Console.WriteLine("Saldo [3]");
+                resp4 = Console.ReadLine();
+
+                if (resp4 == "1")
+                {
+                    Console.WriteLine("Digite o valor do deposito.");
+                    Console.Write("Deposito: R$");
+                    double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    P.Depositar(quantia);
+                    Console.WriteLine("Deposito realizado! Total em Conta: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+                else if (resp4 == "2")
+                {
+                    Console.WriteLine("Digite o valor do saque.");
+                    Console.Write("Saque: R$");
+                    double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    P.Sacar(quantia);
+                    Console.WriteLine("Saque realizado com sucesso! Total em Conta: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+                else if (resp4 == "3")
+                {
+                    Console.WriteLine("Informações de Conta: " + P.nome + ", Conta N° " + NumeroDeConta);
+                    Console.WriteLine("Saldo Atual: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+            }
+            else if (resp3 == "n")
+            {
+                Console.WriteLine("Obrigado. Tenha um bom dia " + P.nome + " :D");
+            }
+            
+            while (resp3 == "s")
+            {
+                Console.WriteLine("Qual operação deseja fazer? ");
+                Console.WriteLine("Deposito [1]");
+                Console.WriteLine("Saque [2]");
+                Console.WriteLine("Saldo [3]");
+                resp4 = Console.ReadLine();
+
+                if (resp4 == "1")
+                {
+                    Console.WriteLine("Digite o valor do deposito.");
+                    Console.Write("Deposito: R$");
+                    double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    P.Depositar(quantia);
+                    Console.WriteLine("Deposito realizado! Total em Conta: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+                else if (resp4 == "2")
+                {
+                    Console.WriteLine("Digite o valor do saque.");
+                    Console.Write("Saque: R$");
+                    double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    P.Sacar(quantia);
+                    Console.WriteLine("Saque realizado com sucesso! Total em Conta: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+                else if (resp4 == "3")
+                {
+                    Console.WriteLine("Informações de Conta: " + P.nome + ", Conta N° " + NumeroDeConta);
+                    Console.WriteLine("Saldo Atual: R$" + P.Saldo);
+                    Console.WriteLine("Deseja fazer/repetir alguma operação? [S/N]");
+                    resp3 = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Obrigado. Tenha um bom dia " + P.nome + " :D");
+            Console.ReadLine();
+        }
+    }
+}
